33ecbae6eb51aa9481cd178c48bda570db0ac351
richisil2005 <166074019+richisil2005@users.noreply.github.com>
2024-06-30 21:54:00 -0500

    Update_3S

--- peliculas.cpp
+++ peliculas.cpp
@@ -2,61 +2,60 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <iostream>
 
-namespace std {
-    unordered_map<string, Pelicula> cargarPeliculas(const string& nombreArchivo) {
-        unordered_map<string, Pelicula> peliculas;
+std::unordered_map<std::string, Pelicula> cargarPeliculas(const std::string& nombreArchivo) {
+    std::unordered_map<std::string, Pelicula> peliculas;
 
-        ifstream archivo(nombreArchivo);
-        if (!archivo.is_open()) {
-            cerr << "Error: No se pudo abrir el archivo." << endl; 
-            return peliculas;
-        }
-
-        string linea;
-        while (getline(archivo, linea)) {
-            stringstream ss(linea);
-            string titulo, sinopsis, tagsStr;
-            getline(ss, titulo, ',');
-            getline(ss, sinopsis, ',');
-            getline(ss, tagsStr, ',');
-
-            Pelicula pelicula;
-            pelicula.titulo = titulo;
-            pelicula.sinopsis = sinopsis;
-
-            stringstream tagsSS(tagsStr);
-            string tag;
-            while (getline(tagsSS, tag, ';')) {
-                pelicula.tags.push_back(tag);
-            }
-
-            peliculas[titulo] = pelicula;
-        }
-
-        archivo.close();
+    std::ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        std::cerr << "Error: No se pudo abrir el archivo." << std::endl;
         return peliculas;
     }
 
-    vector<Pelicula> buscarPeliculas(const unordered_map<string, Pelicula>& peliculas, const string& termino) {
-        vector<Pelicula> resultados;
-        for (const auto& par : peliculas) {
-            const Pelicula& pelicula = par.second;
-            if (pelicula.titulo.find(termino) != string::npos || pelicula.sinopsis.find(termino) != string::npos) {
-                resultados.push_back(pelicula);
-            }
+    std::string linea;
+    while (std::getline(archivo, linea)) {
+        std::stringstream ss(linea);
+        std::string titulo, sinopsis, tagsStr;
+        std::getline(ss, titulo, ',');
+        std::getline(ss, sinopsis, ',');
+        std::getline(ss, tagsStr, ',');
+
+        Pelicula pelicula;
+        pelicula.titulo = titulo;
+        pelicula.sinopsis = sinopsis;
+
+        std::stringstream tagsSS(tagsStr);
+        std::string tag;
+        while (std::getline(tagsSS, tag, ';')) {
+            pelicula.tags.push_back(tag);
         }
-        return resultados;
+
+        peliculas[titulo] = pelicula;
     }
 
-    vector<Pelicula> buscarPorTag(const unordered_map<string, Pelicula>& peliculas, const string& tag) {
-        vector<Pelicula> resultados;
-        for (const auto& par : peliculas) {
-            const Pelicula& pelicula = par.second;
-            if (find(pelicula.tags.begin(), pelicula.tags.end(), tag) != pelicula.tags.end()) {
-                resultados.push_back(pelicula);
-            }
+    archivo.close();
+    return peliculas;
+}
+
+std::vector<Pelicula> buscarPeliculas(const std::unordered_map<std::string, Pelicula>& peliculas, const std::string& termino) {
+    std::vector<Pelicula> resultados;
+    for (const auto& par : peliculas) {
+        const Pelicula& pelicula = par.second;
+        if (pelicula.titulo.find(termino) != std::string::npos || pelicula.sinopsis.find(termino) != std::string::npos) {
+            resultados.push_back(pelicula);
         }
-        return resultados;
     }
+    return resultados;
+}
+
+std::vector<Pelicula> buscarPorTag(const std::unordered_map<std::string, Pelicula>& peliculas, const std::string& tag) {
+    std::vector<Pelicula> resultados;
+    for (const auto& par : peliculas) {
+        const Pelicula& pelicula = par.second;
+        if (std::find(pelicula.tags.begin(), pelicula.tags.end(), tag) != pelicula.tags.end()) {
+            resultados.push_back(pelicula);
+        }
+    }
+    return resultados;
 }
