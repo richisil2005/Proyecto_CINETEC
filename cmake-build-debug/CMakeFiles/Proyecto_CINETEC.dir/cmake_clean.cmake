9012706b046918224d4f0eeb98e40f4b235a5feb
richisil2005 <166074019+richisil2005@users.noreply.github.com>
2024-07-03 21:12:38 -0500

    Update_4S

---
+++ cmake-build-debug/CMakeFiles/Proyecto_CINETEC.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_CINETEC.dir/favoritos.cpp.obj"
+  "CMakeFiles/Proyecto_CINETEC.dir/favoritos.cpp.obj.d"
+  "CMakeFiles/Proyecto_CINETEC.dir/main.cpp.obj"
+  "CMakeFiles/Proyecto_CINETEC.dir/main.cpp.obj.d"
+  "CMakeFiles/Proyecto_CINETEC.dir/peliculas.cpp.obj"
+  "CMakeFiles/Proyecto_CINETEC.dir/peliculas.cpp.obj.d"
+  "Proyecto_CINETEC.exe"
+  "Proyecto_CINETEC.exe.manifest"
+  "Proyecto_CINETEC.pdb"
+  "libProyecto_CINETEC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_CINETEC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
